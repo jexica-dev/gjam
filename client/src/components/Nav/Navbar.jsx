8b5b997f03d97b220fa0baf9db7d57b80a2b99bc
Jexica Ayran <jexica@jexica-mac.local>
2022-12-13 21:36:51 -0500

    correcting navbar size

--- client/src/components/Nav/Navbar.jsx
+++ client/src/components/Nav/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className=" text-sm p-2 mix-blend-color-burn text-white bg-slate-700 w-44 font-gt-mono flex flex-row justify-around">
+    <div className=" text-2xl p-5 mix-blend-color-burn text-white bg-slate-700  w-80 font-gt-mono flex flex-row justify-around">
       <div>
         <Link className=" text-white hover:brightness-200 hover:invert  ">
           JAMS
