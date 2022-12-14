3891f667de72902083524fdf5eb179619820a8eb
Jexica Ayran <jexica@jexica-mac.local>
2022-12-13 21:06:13 -0500

    implementing hide/show sticky navbar on scroll

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -5,38 +5,70 @@ import jamVideo from "../../images/jam.mp4";
 import { Link } from "react-router-dom";
 import banner from "../../images/banner-01.png";
 import Navbar from "../Nav/Navbar";
+import { useState, useEffect, useRef } from "react";
 
 export default function Banner() {
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  // const hideNav = () => {
+  //   window.scrollY > 50 ? setShow(false) : setShow(true);
+  // };
+
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setShow(false);
+      } else {
+        // if scroll up show the navbar
+        setShow(true);
+      }
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
+  const navBar = " ";
+
   return (
     <div className="featured-jams-banner mb-3">
       <div className="">
-        <div className="w-4/5 mix-blend-color-burn fixed flex flex-col items-center top-3 z-50">
+        <div
+          className={`navBar ${
+            show
+              ? " w-4/5 fixed top-3 flex flex-col items-center z-50 mix-blend-color-burn"
+              : " hidden"
+          }`}
+        >
           <Navbar />
         </div>
-        <div className="relative">
-          <img
-            className="filter  mix-blend-color-soft-light  opacity-80  "
-            src={banner}
-            alt=""
-          />
+        <div
+          // style={show ? { marginTop: lastScrollY } : {}}
+          className="relative"
+        >
+          <img src={banner} alt="" />
         </div>
 
-        <div className="w-2/5 lg:w-2/5 md:w-2/5 sm:w-screen absolute top-0 right-0 ">
-          {/* <div className="h-30 bg-white ">test</div> */}
-
+        <div className="w-2/5 lg:w-2/5 md:w-2/5 sm:w-screen absolute top-0 right-0  ">
           <img
             className="p-4 absolute top-0 right-0 filter  mix-blend-hard-light hue-rotate-15 brightness-105 "
             src={gjamLogo}
             alt=""
             srcset=""
           />
-
-          {/* <div className="relative h-44 lg:h-96  md:h-72 sm:h-64">
-              <p className=" text-sm text lg:text-base md:text-sm  absolute font-gt-mono font-bold drop-shadow-lg  bottom-0 lg:left-10 md:left-5  filter mix-blend-soft-light text-black">
-                family-owned, locally grown,{" "}
-                <span className=" block"> and freshly bottled.</span>
-              </p>
-            </div> */}
         </div>
       </div>
     </div>
