6d11d133126b90c576a6c70155e4c2b1fed3a074
Jexica Ayran <jexica@jexica-mac.myfiosgateway.com>
2022-12-16 13:48:43 -0500

    updating deployed db

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -9,7 +9,7 @@ const getToken = () => {
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://gjam-db.herokuapp.com/api"
+      ? "https://db.cybjork.xyz/api"
       : "http://localhost:3000/api",
 });
 
