df146064eb06de6188ce7aefe0fce5882e72179f
jexica ayran <jess.ayran@gmail.com>
2026-02-06 15:55:27 -0800

    add dotenv as first import

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-import db from "./db/connection.js"
-import express from "express"
-import logger from "morgan"
-import cors from "cors"
-import routes from "./routes/index.js"
+import 'dotenv/config';
+import db from './db/connection.js';
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import routes from './routes/index.js';
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(logger("dev"))
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
 
-app.use("/api", routes)
+app.use('/api', routes);
 
-db.on("connected", () => {
-  console.log("Connected to MongoDB!")
+db.on('connected', () => {
+  console.log('Connected to MongoDB!');
   app.listen(PORT, () =>
-    console.log(`Express server application is running on port ${PORT}`)
-  )
-})
+    console.log(`Express server application is running on port ${PORT}`),
+  );
+});
