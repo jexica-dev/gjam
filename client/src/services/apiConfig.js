f5b9dda908d0bf1d21082ae486275a036f9bb3d7
jexica ayran <jess.ayran@gmail.com>
2026-02-06 16:47:46 -0800

    updating api call baseURL

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,27 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
 const getToken = () => {
   return new Promise((resolve) => {
-    resolve(`Bearer ${localStorage.getItem("token") || null}`);
+    resolve(`Bearer ${localStorage.getItem('token') || null}`);
   });
 };
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://db.cybjork.xyz/api"
-      : "http://localhost:3000/api",
+  baseURL: '/api',
 });
 
 api.interceptors.request.use(
   async function (config) {
-    config.headers["Authorization"] = await getToken();
+    config.headers['Authorization'] = await getToken();
     return config;
   },
   function (error) {
-    console.log("Request error: ", error);
+    console.log('Request error: ', error);
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
