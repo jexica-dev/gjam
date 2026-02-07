b99c199dd3c05888d3e4f0d6f9eb9b626958d136
jexica ayran <jess.ayran@gmail.com>
2026-02-06 16:01:43 -0800

    updating react api routes

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
 import routes from './routes/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +20,12 @@ app.use(express.json());
 app.use(logger('dev'));
 
 app.use('/api', routes);
+// Serve React build files
+app.use(express.static(path.join(__dirname, 'client/build')));
+// Catch-all route to serve React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 db.on('connected', () => {
   console.log('Connected to MongoDB!');
